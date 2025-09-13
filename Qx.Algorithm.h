9d6454362454e27ad8f7ad513759a7b800ccaeef
水岷 <2565863219@qq.com>
2025-09-14 03:38:29 +0800

    增加了B2005题目的代码

--- Qx.Algorithm.h
+++ Qx.Algorithm.h
@@ -45,6 +45,17 @@ void P1000() {
 
 void P1001() {
     int a, b; //创建两个整数变量(int) a 和 b
-    std::cin >> a >> b; //使用 cin 读取输入的值 给 a 与 b 进行赋值
+    std::cin >> a >> b; //使用 cin 读取输入的值，给 a 与 b 进行赋值
     std::cout << a + b << std::endl;    // 输出 a + b 的结果
+}
+
+void B2005() {
+    char i; //创建一个字符类型的变量 i 用来存储字符
+
+    std::cin >> i;  //使用 cin 读取输入的值，给 i 赋值
+
+    //按要求输出
+    std::cout << "  " << i << std::endl;
+    std::cout << " " << i << i << i << std::endl;
+    std::cout << i << i << i << i << i << std::endl;
 }
