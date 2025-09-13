4d65354aece3cc313d68d904c4ec94be2cc4b358
水岷 <2565863219@qq.com>
2025-09-14 02:13:30 +0800

    增加了B2025题目的代码

--- Qx.Algorithm.h
+++ Qx.Algorithm.h
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
+
+//题目顺序为洛谷的训练题单
 void B2002() {
 	std::cout << "Hello,World!" << std::endl; //使用 cout 输出 Hello,World! 的内容
+}
+
+void B2025() {
+    //按要求输出，注意空格捏
+    std::cout << "  *" << std::endl;
+    std::cout << " ***" << std::endl;
+    std::cout << "*****" << std::endl;
+    std::cout << " ***" << std::endl;
+    std::cout << "  *" << std::endl;
 }
--- Sm.Algorithm.cpp
+++ Sm.Algorithm.cpp
@@ -1,7 +1,6 @@
 #include "Qx.Algorithm.h"
 
 int main() {
-	B2002();
 
 	return 0;
 }
